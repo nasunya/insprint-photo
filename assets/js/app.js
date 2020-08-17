7ae8cda2e0fa721ef46862bfd62489dacfa1af43
Анастасия Тишина <anastasia@MacBook-Pro-Anastasia.local>
2020-08-17 14:04:01 +0300

    верстка 17/08

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,53 @@
-$(document).ready(function () {
-  let acc = document.getElementsByClassName("answer__acco-question")
-  let i
+// accordion
 
-  for (i = 0; i < acc.length; i++) {
+let acc = document.getElementsByClassName("answer__acco-question")
+let i
 
-    acc[i].addEventListener("click", function () {
-      this.classList.toggle("answer__active");
-      let panel = this.nextElementSibling;
-      if (panel.style.maxHeight) {
-        panel.style.maxHeight = null;
-      } else {
-        panel.style.maxHeight = panel.scrollHeight + "px";
-      }
-    });
+for (i = 0; i < acc.length; i++) {
 
-  }
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("answer__active");
+    let panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
 
+};
+
+
+
+const left = document.querySelector("#left");
+const right = document.querySelector("#right");
+const items = document.querySelector("#items");
+
+const minRight = 0;
+const maxRight = 700;
+const step = 33.3;
+let currentRight = 0; //слайде при загрузке стр всегда вначале
+
+items.style.right = currentRight;
+
+
+right.addEventListener("click", function () {
+  event.preventDefault();
+  if (currentRight < maxRight) {
+    currentRight += step; //currentRight      +step
+    items.style.right = currentRight + "%";
+  } else {
+    currentRight = 0 // постоянная прокрутка
+    items.style.right = 0 //
+  };
+});
+left.addEventListener("click", function () {
+  event.preventDefault();
+  if (currentRight > minRight) {
+    currentRight -= step;
+    items.style.right = currentRight + "%";
+  } else {
+    currentRight = maxRight;
+    items.style.right = currentRight + "%";
+  };
 });
