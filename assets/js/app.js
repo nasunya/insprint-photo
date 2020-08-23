1a6e6ffa30fec93b0b50482304f9c4936b9f7922
Анастасия Тишина <anastasia@192.168.1.103>
2020-08-23 15:34:23 +0300

    верстка 22/08

--- assets/js/app.js
+++ assets/js/app.js
@@ -124,3 +124,14 @@ photogBtn.addEventListener("click", function () {
   photogBlock.style.display = "flex";
   amateurBlock.style.display = "none";
 });
+
+
+const jsMenu = document.querySelector(".burger__btn")
+const nav = document.querySelector(".burger__menu")
+
+
+jsMenu.onclick = function () {
+  this.classList.toggle('burger__opened');
+  this.classList.contains('burger__opened');
+  nav.classList.toggle("burger__menu--active");
+};
